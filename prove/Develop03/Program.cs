6102f5ce026edcc38d62b1130788b9a01134bba8
adamgsheridan <adamgsheridan7@gmail.com>
2023-10-21 23:18:29 -0600

    .

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture();
+        scripture.PrintScripture();
+
+        Prompt prompt = new Prompt();
+        prompt.PromptContinue();
+
+        while(prompt.ShouldContinue() && scripture.HasWordToHide())
+        {
+            Console.Clear();
+            scripture.WriteScriptureWithHiddenWords();
+            prompt.PromptContinue();
+        }
     }
 }
